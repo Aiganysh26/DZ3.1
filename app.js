ca634a93434c68da6f5dbed868ecf7e839ab49c5
Aiganysh26 <aiganyshbeisheeva7@gmail.com>
2023-04-14 18:23:48 +0200

    second commit

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
-var num = prompt('Введите число')
 for(var i = 0; i <= 100; i++){
-  if(num % 3 === 0 && num % 5 !== 0){
-    console.log('Fizz')
-    continue
+    if(i % 3 === 0 && i % 5 !== 0){
+      console.log('Fizz')
+      continue
+    }
+    else if(i % 5 === 0 && i % 3 !== 0){
+      console.log('Buzz')
+      continue
+    }
+    else if(i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz')
+    }else{
+      console.log(i)
+    }
   }
-  else if(num % 5 === 0 && num % 3 !== 0){
-    console.log('Buzz')
-    continue
-  }
-  else if(num % 3 === 0 && num % 5 === 0) {
-    console.log('FizzBuzz')
-  }else{
-    console.log('error')
-  }
-}
